0ec5a936ce290f25b8d223db86ddb17408d0e7c9
George Bakalis <36627616+georgebak2182@users.noreply.github.com>
2019-09-05 15:08:25 +0300

    Update login-system-getpass.py

--- login-system-getpass.py
+++ login-system-getpass.py
@@ -1,15 +1,15 @@
 import getpass
 print "Coded by George Bakalis"
 print ("Login System")
-Swsto_Username = "Your_username"
-Swsto_Password = "Your_password"
+Correct_Username = "Your_username"
+Correct_Password = "Your_password"
  
 loop = 'true'
 while (loop == 'true'):
     username = raw_input("Enter your username: ")
-    if (username == Swsto_Username):
+    if (username == Correct_Username):
         password = getpass.getpass("Enter password for user %s: " % Swsto_Username)
-        if (password == Swsto_Password):
+        if (password == Correct_Password):
             print "Successful login as " + username
             loop = 'false'
         else:
